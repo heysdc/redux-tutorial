6eb6d05f82bb7a2988553e81e3622e971ffed29b
sdc <heysdc@163.com>
2016-10-14 11:26:08 +0800

    Provider

--- client/index.js
+++ client/index.js
@@ -1,31 +1,31 @@
 /* global store */
 
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
 import todoApp from './reducers'
 import * as actions from './actions'
 window.actions = actions
 window.reducers = todoApp
 
-// // 自实现createStore
-// window.createStore = function (reducer) {
-//   var store = reducer(undefined, {type: undefined})
-//   var subscribe
+// 自实现createStore
+window.createStore = function (reducer) {
+  var store = reducer(undefined, {type: undefined})
+  var subscribe
 
-//   var currentStore = {
-//     getState: function () {
-//       return store
-//     },
-//     subscribe: function (callback) {
-//       subscribe = callback
-//     },
-//     dispatch: function (action) {
-//       store = reducer(store, action)
-//       typeof subscribe === 'function' && subscribe()
-//     }
-//   }
+  var currentStore = {
+    getState: function () {
+      return store
+    },
+    subscribe: function (callback) {
+      subscribe = callback
+    },
+    dispatch: function (action) {
+      store = reducer(store, action)
+      typeof subscribe === 'function' && subscribe()
+    }
+  }
 
-//   return currentStore
-// }
+  return currentStore
+}
 
 // 创建store
 window.store = createStore(todoApp)
@@ -39,88 +39,102 @@ store.subscribe(() => {
 // 更改store
 store.dispatch(actions.addArticle('aaa'))
 
-// // React例子
-// import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-// import { Provider } from 'react-redux'
+// React例子
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+// import { Provider, connect } from 'react-redux'
 
-// // 自实现connect
-// const connect = (mapStateToProps, mapDispatchToProps) => (Comp) => {
-//   class Connect extends Component {
-//     state = {
-//       store: this.context.store.getState()
-//     }
-//     componentDidMount = () => {
-//       this.context.store.subscribe(() => {
-//         const store = this.context.store.getState()
-//         this.setState({
-//           store
-//         })
-//       })
-//     }
+// 自实现Provider
+class Provider extends Component {
+  getChildContext = () => {
+    return {store: this.props.store}
+  }
+  render() {
+    return React.Children.only(this.props.children)
+  }
+}
 
-//     render () {
-//       const store = this.state.store
-//       return <Comp
-//         {
-//           ...mapStateToProps(store)
-//         }
-//         {
-//           ...mapDispatchToProps(this.context.store.dispatch)
-//         }
-//       />
-//     }
-//   }
-//   Connect.contextTypes = {
-//     store: React.PropTypes.object
-//   }
-//   return Connect
-// }
+Provider.childContextTypes = {
+  store: React.PropTypes.object
+}
 
-// const AddTodo = ({articles, addArticle}) => {
-//   const add = (e) => {
-//     e.preventDefault()
-//     addArticle(inputNode.value)
-//   }
-//   let inputNode = null
-//   return <div>
-//     <ul>
-//       {
-//         articles.map((value, id) => (<li key={id}>{value}</li>))
-//       }
-//     </ul>
-//     <form onSubmit={add}>
-//       <input ref={(node) => { inputNode = node }} />
-//       <button type='sbumit'>添加</button>
-//     </form>
-//   </div>
-// }
+// 自实现connect
+const connect = (mapStateToProps, mapDispatchToProps) => (Comp) => {
+  class Connect extends Component {
+    state = {
+      store: this.context.store.getState()
+    }
+    componentDidMount = () => {
+      this.context.store.subscribe(() => {
+        const store = this.context.store.getState()
+        this.setState({
+          store
+        })
+      })
+    }
 
-// const mapDispatchToProps = (dispatch, ownProps) => {
-//   return {
-//     addArticle: (inputValue) => {
-//       dispatch(actions.addArticle(inputValue))
-//     }
-//   }
-// }
+    render () {
+      const store = this.state.store
+      return <Comp
+        {
+          ...mapStateToProps(store)
+        }
+        {
+          ...mapDispatchToProps(this.context.store.dispatch)
+        }
+      />
+    }
+  }
+  Connect.contextTypes = {
+    store: React.PropTypes.object
+  }
+  return Connect
+}
 
-// const mapStateToProps = (state, ownProps) => {
-//   return {
-//     articles: state.articles
-//   }
-// }
+const AddTodo = ({articles, addArticle}) => {
+  const add = (e) => {
+    e.preventDefault()
+    addArticle(inputNode.value)
+  }
+  let inputNode = null
+  return <div>
+    <ul>
+      {
+        articles.map((value, id) => (<li key={id}>{value}</li>))
+      }
+    </ul>
+    <form onSubmit={add}>
+      <input ref={(node) => { inputNode = node }} />
+      <button type='sbumit'>添加</button>
+    </form>
+  </div>
+}
 
-// const ReactDemo = connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(AddTodo)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    addArticle: (inputValue) => {
+      dispatch(actions.addArticle(inputValue))
+    }
+  }
+}
 
-// class World extends Component {
-//   render () {
-//     return <Provider store={store}>
-//       <ReactDemo />
-//     </Provider>
-//   }
-// }
+const mapStateToProps = (state, ownProps) => {
+  return {
+    articles: state.articles
+  }
+}
 
-// ReactDOM.render(<World />, document.getElementById('root'))
+const ReactDemo = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddTodo)
+
+class World extends Component {
+  render () {
+    return <Provider store={store}>
+      <ReactDemo />
+    </Provider>
+  }
+}
+
+ReactDOM.render(<World />, document.getElementById('root'))
